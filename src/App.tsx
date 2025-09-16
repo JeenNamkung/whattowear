016e5b60d94e2d02d803c5d3ca2e24d397b68704
Jeen Namkung <jnamkung@my.bcit.ca>
2025-09-16 14:58:25 -0700

    chore: clean up unused files and configure basic app settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
+    <div className="w-full min-h-screen flex items-center justify-center">
       <p className='font-bold text-4xl'>
         Tailwind test
       </p>
-    </>
+    </div>
   )
 }
-
-export default App
