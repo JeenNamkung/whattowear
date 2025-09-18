93dc29f40217f970895212172661ebb4b8a15823
Jeen Namkung <jnamkung@my.bcit.ca>
2025-09-18 12:17:01 -0700

    feat: add ClothingRecommendation type

---
+++ src/types/recommendationType.ts
@@ -0,0 +1,9 @@
+export interface ClothingRecommendation {
+  text: string;
+  subText?: string;
+  top: string;
+  bottom: string;
+  footwear: string;
+  outerwear?: string;
+  accessories?: string[];
+}
