22dc116563c44b1fe7410c5613ff8e59abfc25b9
Jeen Namkung <jnamkung@my.bcit.ca>
2025-09-18 12:31:14 -0700

    chore: add images.ts to consolidate image imports/exports

---
+++ src/constants/images.ts
@@ -0,0 +1,47 @@
+import Cardigan from "../assets/clothing/cardigan.png";
+import Crocs from "../assets/clothing/crocs.png";
+import Gloves from "../assets/clothing/gloves.png";
+import Hoodie from "../assets/clothing/hoodie.png";
+import Jacket from "../assets/clothing/jacket.png";
+import Jeans from "../assets/clothing/jeans.png";
+import Pants from "../assets/clothing/pants.png";
+import Rainboots from "../assets/clothing/rainboots.png";
+import Raincoat from "../assets/clothing/raincoat.png";
+import Scarf from "../assets/clothing/scarf.png";
+import Shirt from "../assets/clothing/shirt.png";
+import Shoes from "../assets/clothing/shoes.png";
+import Sweater from "../assets/clothing/sweater.png";
+import Tanktop from "../assets/clothing/tanktop.png";
+import Thermalpants from "../assets/clothing/thermalpants.png";
+import Trainingpants from "../assets/clothing/trainingpants.png";
+import Tshirt from "../assets/clothing/tshirt.png";
+import Umbrella from "../assets/clothing/umbrella.png";
+import Windbreaker from "../assets/clothing/windbreaker.png";
+import Winterboots from "../assets/clothing/winterboots.png";
+import Winterhat from "../assets/clothing/winterhat.png";
+import Winterjacket from "../assets/clothing/winterjacket.png";
+
+export const images = {
+  Cardigan,
+  Crocs,
+  Gloves,
+  Hoodie,
+  Jacket,
+  Jeans,
+  Pants,
+  Rainboots,
+  Raincoat,
+  Scarf,
+  Shirt,
+  Shoes,
+  Sweater,
+  Tanktop,
+  Thermalpants,
+  Trainingpants,
+  Tshirt,
+  Umbrella,
+  Windbreaker,
+  Winterboots,
+  Winterhat,
+  Winterjacket,
+}
