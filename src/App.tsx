d7b220d21fae0102ffbab8887b9a0f0d65f33055
Jeen Namkung <jnamkung@my.bcit.ca>
2025-09-18 12:55:53 -0700

    feat: render HomePage in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import HomePage from "./pages/HomePage";
+
 export default function App() {
   return (
-    <div className="w-full min-h-screen flex items-center justify-center">
-      <p className='font-bold text-4xl'>
-        Tailwind test
-      </p>
-    </div>
+    <HomePage />
   )
 }
